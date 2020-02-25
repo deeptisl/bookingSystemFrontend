4327b9a16f6f2f345cf66489aeaa55f94c6e8a3e
deeptisl <41547887+deeptisl@users.noreply.github.com>
2020-02-26 01:42:39 +0530

    Changes Due to New page redirection

--- src/component/App/App.jsx
+++ src/component/App/App.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import { Route } from 'react-router-dom';
 import Login from '../Login/Login';
+import BookingPage from '../Booking/BookingForm';
 import './App.css';
 
 const App = () => (
   <div>
     <Route exact path="/" component={Login} />
+    <Route exact path="/BookingFrom" component={BookingPage} />
     {/* <Route path="/HomePage" component={HomePage} />
     <Route path="/ForgotPassword" component={ForgotPassword} />
     <Route path="/EnterOTP" component={EnterOTP} />
